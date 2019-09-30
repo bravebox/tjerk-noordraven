2a28120bd6781be510a2d479afd0bd560faec415
Mick Schouten <bravebox@gmail.com>
2019-09-30 16:06:32 +0200

    lastest changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { SiteProvider } from "./src/context/SiteContext"
-import "./src/scripts/bats.js"
+import "./src/scripts/custom-scripts.js"
 
 export const wrapRootElement = ({ element }) => (
   <SiteProvider>{element}</SiteProvider>
--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -4,20 +4,38 @@ import Footer from "../components/Footer"
 import HeaderPage from "../components/HeaderPage"
 import "../styles/main.scss"
 
-export default ({ children }) => (
-  <div className="vs-site-wrap">
-    <div className="vs-site">
-      <Nav />
-      <div className="vs-content-container">
-        <div className="vs-pageloader" hidden>
-          <img src="/img/status.gif" alt="status.gif" />
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLoader: true
+    }
+
+    setTimeout(() => {
+      this.setState({ showLoader: false });
+    }, 1200);
+  }
+
+  render() {
+    return (
+      <div className="vs-site-wrap">
+        <div className="vs-site">
+          <Nav />
+          <div className="vs-content-container">
+          <div className={`vs-pageloader ${this.state.showLoader ? 'is-active' : 'is-not-active'}`}>
+              <img src="/img/status.gif" alt="status.gif" />
+            </div>
+            <HeaderPage />
+            <div className="vs-content-inner-container mobile-center-text">
+              {this.props.children}
+            </div>
+          </div>
         </div>
-        <HeaderPage />
-        <div className="vs-content-inner-container mobile-center-text">
-          {children}
-        </div>
+        <Footer />
       </div>
-    </div>
-    <Footer />
-  </div>
-)
+    )
+  }
+}
+
+export default props => <Page {...props} />
