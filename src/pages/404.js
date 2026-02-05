cb9555fd2ccf67b4f35ee28151aabfeba568ca39
= <bravebox@gmail.com>
2026-02-05 08:43:11 +0100

    Updated 404 page with improved styling and Dutch content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,32 @@
 import React from "react"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import Nav from "../components/Nav"
 import Page from "../layouts/Page"
 
-class default404 extends React.Component {
-  render() {
-    return(
-      <React.Fragment>
-        <Page>
-          <Nav />
-          <p>Page not found</p>
-        </Page>
-      </React.Fragment>
-    );
-  }
+const NotFoundPage = () => {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pagina niet gevonden - Tjerk Noordraven</title>
+        <meta name="robots" content="noindex" />
+        <link rel="canonical" href="http://www.tjerknoordraven.com/404/" />
+      </Helmet>
+      <Page>
+        <Nav />
+        <section className="vs-section vs-section--rock u-text--c u-pt-4 u-pb-4">
+          <h1>404 - Pagina niet gevonden</h1>
+          <p>Deze pagina bestaat niet (meer).</p>
+          <div className="u-mt-2">
+            <Link to="/" className="u-btn-ghost">
+              <span>Terug naar Boeken</span>
+            </Link>
+          </div>
+        </section>
+      </Page>
+    </React.Fragment>
+  )
 }
 
-export default default404
+export default NotFoundPage
